1172b46a83bd3a1971664d9ff0d7156f64a739f9
EvgeniyK6 <evgeniy.kozovyakin@gmail.com>
2021-01-03 14:08:24 +0200

    initial

---
+++ DogT/Data/DogTContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DogT.Models;
+
+namespace DogT.Data
+{
+    public class DogTContext : DbContext
+    {
+        public DogTContext(DbContextOptions<DogTContext> options) : base(options)
+        {
+        }
+
+        DbSet<User> Users { get; set; }
+    }
+}
